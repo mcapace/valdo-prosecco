8f47b25360cf7711b0c5bc002e18aeead085c8bb
mcapace <mike.capace@gmail.com>
2025-08-12 10:31:10 +0200

    Increase bottom OCM logo size for better readability

--- src/components/sections/OCMLogoSection.tsx
+++ src/components/sections/OCMLogoSection.tsx
@@ -27,9 +27,9 @@ const OCMLogoSection = () => {
             <Image
               src="/images/Logos/OCM_Logo__1_-removebg-preview.png"
               alt="OCM Logo"
-              width={100}
-              height={50}
-              className="w-auto h-8 lg:h-10"
+              width={120}
+              height={60}
+              className="w-auto h-10 lg:h-12"
             />
           </motion.div>
           <motion.p 
